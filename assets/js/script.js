9e7b6e73f85335beb762ea404b10efd993af5392
mendezhector16 <mendezhector16@gmail.com>
2022-06-15 20:49:21 -0500

    timeblocks will change colors depending on the past present or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
 //display current date 
 var todaysDateEl = moment().format("MMMM-DD-YYYY");
+var currentTime = moment().hour();
+var tasks = [];
 
 //append date to the top of calendar
 $("#currentDay").html(todaysDateEl);
 
+//save tas to local storage
+var saveTasks = function() {
+    localStorage.setItem()
+}
+//color code time slots depending on past, present, or future
+//loop throght the time
+$(".time-block").each(function() {
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 
-//blocks of time for standard business hours (9-5pm)
+    if (timeBlock === currentTime) {
+        $(this).addClass("present");
+    } 
+    else if (timeBlock > currentTime) {
+        $(this).addClass("future");   
+    }
+    else {
+        $(this).addClass("past");
+    };
+});
