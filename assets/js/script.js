593333e60fef983ac674ef4fbec07a33ef80e433
mendezhector16 <mendezhector16@gmail.com>
2022-06-15 20:57:32 -0500

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,12 @@
 //display current date 
 var todaysDateEl = moment().format("MMMM-DD-YYYY");
 var currentTime = moment().hour();
-var tasks = [];
 
 //append date to the top of calendar
 $("#currentDay").html(todaysDateEl);
 
-//save tas to local storage
-var saveTasks = function() {
-    localStorage.setItem()
-}
+
 //color code time slots depending on past, present, or future
-//loop throght the time
 $(".time-block").each(function() {
     var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 
@@ -25,3 +20,4 @@ $(".time-block").each(function() {
         $(this).addClass("past");
     };
 });
+
